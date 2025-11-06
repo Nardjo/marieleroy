3dc7dc738db0230ae878433527c8a4473bf7f531
nardjo <jord.bastin@gmail.com>
2025-11-06 15:02:21 +0100

    update: add primary border to cards

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   ui: {
     card: {
       slots: {
-        root: '!bg-card',
+        root: '!bg-card border border-primary-600',
         header: 'px-6 py-4 border-b border-gray-200 dark:border-gray-700',
         body: 'px-6 py-6 sm:px-8 sm:py-8',
         footer: 'px-6 py-4 border-t border-gray-200 dark:border-gray-700',

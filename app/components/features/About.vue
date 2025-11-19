3e83b8ebe8b63efed46db056da97585175a74a27
nardjo <jord.bastin@gmail.com>
2025-11-19 18:25:48 +0100

    fix: force background color on about card

--- app/components/features/About.vue
+++ app/components/features/About.vue
@@ -30,7 +30,7 @@
         v-motion
         :initial="{ opacity: 0, y: 50 }"
         :visible-once="{ opacity: 1, y: 0, transition: { duration: 600, delay: 200 } }">
-        <UPageCard spotlight class="shadow-lg bg-primary-100 [--spotlight-color:#8b6239] [--spotlight-size:600px]">
+        <UPageCard spotlight class="shadow-lg !bg-primary-100 [--spotlight-color:#8b6239] [--spotlight-size:600px]">
           <div class="flex flex-col-reverse md:flex-row gap-8 md:gap-12 items-center p-6">
             <!-- Texte à gauche -->
             <div class="flex-1 space-y-6">

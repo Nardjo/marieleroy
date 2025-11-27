b74760623d9578e5a69d85604a6c62cadd5e3db1
nardjo <jord.bastin@gmail.com>
2025-11-27 16:36:38 +0100

    fix: enforce dancing script normal weight

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <h2
-    class="font-dancing-script drop-shadow-md text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-primary-700"
+    class="font-dancing-script font-normal drop-shadow-md text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-primary-700"
     :class="{ 'min-w-fit min-h-30 whitespace-nowrap': !compact, 'leading-none': compact }"
     :style="{ fontSize: props.size + 'px' }">
     {{ minimal ? 'ML' : 'Marie Leroy' }}
